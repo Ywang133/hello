c7af8091710ac6b6bce8c17894f7534888b32bbd
Ywang133 <46725515+Ywang133@users.noreply.github.com>
2019-01-17 13:20:34 -0600

    Create Hello.c

---
+++ Hello.c
@@ -0,0 +1,19 @@
+/**
+ * Author: Yazhe Wang
+ * Date: 2019/01/17
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("yazhe wang  Computer Science \n");
+  printf("Wendian Zhang  Computer Science \n");
+
+
+
+  return 0;
+}
